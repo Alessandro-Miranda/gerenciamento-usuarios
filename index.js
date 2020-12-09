e9df81d56687aec5eaa0357a3923f8ad61fca52a
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2020-12-09 16:31:28 -0300

    Inserido foreach para iniciar o novo tópico

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ var country = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
-// var admin = document.querySelector("#");
+var admin = document.querySelector("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(fields => {
+    console.log(fields)
+});
