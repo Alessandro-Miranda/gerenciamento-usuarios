85f80c2c0ba75d97774a73fb7b063fed31b1aa07
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2020-12-09 16:01:51 -0300

    Alterado nome dos inputs e modificado as variaveis

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var valor = document.querySelector("#exampleInputName");
-var generoM = document.querySelector("#exampleInputGenderM");
-var generoF = document.querySelector("#exampleInputGenderF");
-var nascimento = document.querySelector("#exampleInputBirth");
-var pais = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var senha = document.querySelector("#exampleInputPassword1");
-var file = document.querySelector("#exampleInputFile");
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+// var admin = document.querySelector("#");
