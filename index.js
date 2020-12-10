ce96df314c159a1e419ad043929124a7db339a7f
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2020-12-10 11:04:31 -0300

    Modificado para gerar a tr diretamente no tbody

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ var user = {};
 
 function addLine(dataUser)
 {
-    var tr = document.createElement("tr");
-    tr.innerHTML = `<tr>
-                        <td>
-                            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-                        </td>
-                        <td>${dataUser.name}</td>
-                        <td>${dataUser.email}</td>
-                        <td>${dataUser.admin}</td>
-                        <td>${dataUser.date}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    </tr>`;
-
-    document.getElementById("tableUsers").appendChild(tr);
+    document.getElementById("tableUsers").innerHTML += `
+                                                        <td>
+                                                            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                                                        </td>
+                                                        <td>${dataUser.name}</td>
+                                                        <td>${dataUser.email}</td>
+                                                        <td>${dataUser.admin}</td>
+                                                        <td>${dataUser.date}</td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                                                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                                                        </td>
+                                                    `;
 }
 
 document.getElementById("form-user-create").addEventListener("submit", function(e){
