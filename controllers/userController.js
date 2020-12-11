05df6788bf1c6a2e40e7acba7085b06814da59fb
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2020-12-11 08:21:23 -0300

    Modificado de callback para promise

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,27 +15,33 @@ class UserController
 
             let values = this.getValues();
 
-            this.getPhoto((content) => {
+            this.getPhoto().then((content) => {
                 values.photo = content;
                 this.addLine(this.getValues())
+            },
+            (e) => {
+                console.error(e);
             });
-
         });
     }
-    getPhoto(callback)
+    getPhoto()
     {
-        let fileReader = new FileReader();
-
-        let elements = [...this.formEl.elements].filter(item => {
-            return item.name === 'photo' && item;
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+    
+            let elements = [...this.formEl.elements].filter(item => {
+                return item.name === 'photo' && item;
+            });
+            let file = elements[0].files[0];
+    
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (e) => {
+                reject(e);
+            }
+            fileReader.readAsDataURL(file);
         });
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-            callback(fileReader.result);
-        };
-        
-        fileReader.readAsDataURL(file);
     }
     getValues()
     {
